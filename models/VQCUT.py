730b9494a72fb149a1b06b4656f36b5f3bdf7838
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-06 08:49:31 +0800

    Update VQCUT.py

--- models/VQCUT.py
+++ models/VQCUT.py
@@ -1,1 +1,29 @@
+from modules import *
+from losses import *
+import tensorflow as tf
+from tensorflow.keras import layers
 
+
+class VectorQuantizer(layers.Layer):
+  def __init__(self, config):
+    super().__init__()
+    
+  def call(self, x):
+    return 
+  
+
+class Encoder(tf.keras.Model):
+  pass
+
+
+class Decoder(tf.keras.Model):
+  pass
+
+
+class Generator(tf.keras.Model):
+  pass
+
+
+
+class VQCUT(tf.keras.Model):
+  pass
