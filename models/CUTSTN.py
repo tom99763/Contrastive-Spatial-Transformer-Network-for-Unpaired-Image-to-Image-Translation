f7603dedcd75382fc03dee5cd712c1e83f616f69
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-10 13:07:42 +0800

    Create CUTSTN.py

---
+++ models/CUTSTN.py
@@ -0,0 +1,17 @@
+from modules import *
+import tensorflow as tf
+
+class UNET(tf.keras.Model):
+  pass
+
+class TextureSynthsizer(tf.keras.Model):
+  pass
+
+class PatchSampler:
+  pass
+
+class STN(tf.keras.Model):
+  pass
+
+class CUTSTN(tf.keras.Model):
+  pass
