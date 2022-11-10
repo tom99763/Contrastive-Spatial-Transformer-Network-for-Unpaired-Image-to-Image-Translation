fb67bf97c0d6a7c91be418be848918e79f9b0cd9
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-10 17:32:59 +0800

    Update CUTSTN.py

--- models/CUTSTN.py
+++ models/CUTSTN.py
@@ -4,6 +4,7 @@ from modules import *
 from discriminators import *
 from tensorflow.keras import initializers, layers
 
+
 class BilinearSampler(layers.Layer):
     def __init__(self):
         super().__init__()
@@ -39,7 +40,7 @@ class BilinearSampler(layers.Layer):
         homogenous_coordinates = tf.tile(homogenous_coordinates, [batch, 1, 1])
         homogenous_coordinates = tf.cast(homogenous_coordinates, dtype=tf.float32)
         return homogenous_coordinates
-    
+
     def interpolate(self, images, homogenous_coordinates, theta):
         with tf.name_scope("Transformation"):
             transformed = tf.matmul(theta, homogenous_coordinates)
@@ -90,101 +91,102 @@ class BilinearSampler(layers.Layer):
         return tf.math.add_n([wa * Ia + wb * Ib + wc * Ic + wd * Id])
 
 
-
 class STN(tf.keras.Model):
-  def __init__(self, config):
-    super().__init__()
-    self.config = config
-    self.localizer = build_localizer()
-    self.sampler = BilinearSampler()
-    self.act = config['act']
-    self.use_bias = config['use_bias']
-    self.norm = config['norm']
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.localizer = build_localizer()
+        self.sampler = BilinearSampler()
+        self.act = config['act']
+        self.use_bias = config['use_bias']
+        self.norm = config['norm']
 
-  def call(self, x):
-    theta = self.localizer(x) #(b, 2, 3)
-    x = self.sampler([x, theta])
-    return x
-  
-  def build_localizer(self):
-    dim = self.config['base']
-    blocks = tf.keras.Sequential([
-        layers.Input([None, None, 3])
-        Padding2D(3, pad_type='reflect'),
-        ConvBlock(dim, 7, padding='valid', use_bias=self.use_bias, norm_layer=self.norm, activation=self.act),
-    ])
-    
-    for _ in range(4):
-        dim = min(dim  * 2, self.config['max_filters'])
-        self.blocks.add(ConvBlock(dim, 3, strides=2, padding='same',
-                                use_bias=self.use_bias, norm_layer=self.norm, activation=self.act))
-    self.blocks.add(layers.Flatten())
-    self.blocks.add(LinearBlock(self.config['max_filters']), activation = self.act)
-    self.blocks.add(layers.Dense(layers.Dense(
-        units=6,   
-        bias_initializer=initializers.constant([1.0, 0.0, 0.0, 0.0, 1.0, 0.0]), #initialize as A = [I, t]
-        kernel_initializer='zeros')))
-    self.blocks.add(layers.Reshape((2, 3)))
-    return blocks
+    def call(self, x):
+        theta = self.localizer(x)  # (b, 2, 3)
+        x = self.sampler([x, theta])
+        return x
 
-  
+    def build_localizer(self):
+        dim = self.config['base']
+        blocks = tf.keras.Sequential([
+            layers.Input([None, None, 3]),
+            Padding2D(3, pad_type='reflect'),
+            ConvBlock(dim, 7, padding='valid', use_bias=self.use_bias, norm_layer=self.norm, activation=self.act),
+        ])
+
+        for _ in range(4):
+            dim = min(dim * 2, self.config['max_filters'])
+            blocks.add(ConvBlock(dim, 3, strides=2, padding='same',
+                                      use_bias=self.use_bias, norm_layer=self.norm, activation=self.act))
+        blocks.add(layers.Flatten())
+        blocks.add(LinearBlock(self.config['max_filters']), activation=self.act)
+        blocks.add(layers.Dense(layers.Dense(
+            units=6,
+            bias_initializer=initializers.constant([1.0, 0.0, 0.0, 0.0, 1.0, 0.0]),  # initialize as A = [I, t]
+            kernel_initializer='zeros')))
+        blocks.add(layers.Reshape((2, 3)))
+        return blocks
+
+
 class Generator(tf.keras.Model):
-  def __init__(self, config):
-    super().__init__()
-    self.act = config['act']
-    self.use_bias = config['use_bias']
-    self.norm = config['norm']
-    self.num_downsampls = config['num_downsamples']
-    self.num_resblocks = config['num_resblocks']
-    dim = config['base']
-    
-    #build generator
-    self.blocks = tf.keras.Sequential([
-      layers.Input([None, None, 6])
-      Padding2D(3, pad_type='reflect'),
-      ConvBlock(dim, 7, padding='valid', use_bias=self.use_bias, norm_layer=self.norm, activation=self.act),
-    ])
-    
-    for _ in range(self.num_downsampls):
-      dim = dim  * 2
-      self.blocks.add(ConvBlock(dim, 3, strides=2, padding='same',
-                                use_bias=self.use_bias, norm_layer=self.norm, activation=self.act))
-      
-    for _ in range(self.num_resblocks):
-      self.blocks.add(ResBlock(dim, 3, self.use_bias, self.norm))
-      
-    for _ in range(self.num_downsampls):
-      dim  = dim / 2
-      self.blocks.add(ConvTransposeBlock(dim, 3, strides=2, padding='same', use_bias=self.use_bias, norm_layer=self.norm, activation=self.act))
-    self.blocks.add(Padding2D(3, pad_type='reflect'))
-    self.blocks.add(ConvBlock(3, 7, padding='valid', activation='tanh'))
-    
-    #build encoder
-    self.E = self.build_encoder()
-    
-    #build spatial transformer
-    self.stn = STN(config)
-    
-    #set config
-    self.config=config
-    self.scale = self.add_weight(shape=(1,), initializer = initializers.Constant(0.1), trainable=True)
-    
-  def call(self, inputs):
-    x, z = inputs
-    x = self.wrap(x)
-    x = tf.concat([x, self.scale * z], axis=-1)
-    x = self.blocks(x)
-    return x
-    
-  def wrap(self, x):
-    return self.stn(x)
-    
-  def build_encoder(self):
-    nce_layers = self.config['nce_layers']
-    outputs = []
-    for idx in nce_layers:
-      outputs.append(self.layers[idx].output)
-    return tf.keras.Model(inputs=self.input, outputs=outputs)
+    def __init__(self, config):
+        super().__init__()
+        self.act = config['act']
+        self.use_bias = config['use_bias']
+        self.norm = config['norm']
+        self.num_downsampls = config['num_downsamples']
+        self.num_resblocks = config['num_resblocks']
+        dim = config['base']
+
+        # build generator
+        self.blocks = tf.keras.Sequential([
+            layers.Input([None, None, 6]),
+            Padding2D(3, pad_type='reflect'),
+            ConvBlock(dim, 7, padding='valid', use_bias=self.use_bias, norm_layer=self.norm, activation=self.act),
+        ])
+
+        for _ in range(self.num_downsampls):
+            dim = dim * 2
+            self.blocks.add(ConvBlock(dim, 3, strides=2, padding='same',
+                                      use_bias=self.use_bias, norm_layer=self.norm, activation=self.act))
+
+        for _ in range(self.num_resblocks):
+            self.blocks.add(ResBlock(dim, 3, self.use_bias, self.norm))
+
+        for _ in range(self.num_downsampls):
+            dim = dim / 2
+            self.blocks.add(
+                ConvTransposeBlock(dim, 3, strides=2, padding='same', use_bias=self.use_bias, norm_layer=self.norm,
+                                   activation=self.act))
+        self.blocks.add(Padding2D(3, pad_type='reflect'))
+        self.blocks.add(ConvBlock(3, 7, padding='valid', activation='tanh'))
+
+        # build encoder
+        self.E = self.build_encoder()
+
+        # build spatial transformer
+        self.stn = STN(config)
+
+        # set config
+        self.config = config
+        self.scale = self.add_weight(shape=(1,), initializer=initializers.Constant(0.1), trainable=True)
+
+    def call(self, inputs):
+        x, z = inputs
+        x = self.wrap(x)
+        x = tf.concat([x, self.scale * z], axis=-1)
+        x = self.blocks(x)
+        return x
+
+    def wrap(self, x):
+        return self.stn(x)
+
+    def build_encoder(self):
+        nce_layers = self.config['nce_layers']
+        outputs = []
+        for idx in nce_layers:
+            outputs.append(self.layers[idx].output)
+        return tf.keras.Model(inputs=self.input, outputs=outputs)
 
 
 class PatchSampler(tf.keras.Model):
@@ -192,16 +194,17 @@ class PatchSampler(tf.keras.Model):
         super(PatchSampleMLP, self).__init__(**kwargs)
         self.units = config['units']
         self.num_patches = config['num_patches']
-        self.l2_norm = layers.Lambda(lambda x: x * tf.math.rsqrt(tf.reduce_sum(tf.square(x), axis=-1, keepdims=True) + 1e-10))
+        self.l2_norm = layers.Lambda(
+            lambda x: x * tf.math.rsqrt(tf.reduce_sum(tf.square(x), axis=-1, keepdims=True) + 1e-10))
 
     def build(self, input_shape):
         initializer = tf.random_normal_initializer(0., 0.02)
         feats_shape = input_shape
         for feat_id in range(len(feats_shape)):
             mlp = tf.keras.models.Sequential([
-                    layers.Dense(self.units, activation="relu", kernel_initializer=initializer),
-                    layers.Dense(self.units, kernel_initializer=initializer),
-                ])
+                layers.Dense(self.units, activation="relu", kernel_initializer=initializer),
+                layers.Dense(self.units, kernel_initializer=initializer),
+            ])
             setattr(self, f'mlp_{feat_id}', mlp)
 
     def call(self, inputs, patch_ids=None, training=None):
@@ -223,60 +226,61 @@ class PatchSampler(tf.keras.Model):
             ids.append(patch_id)
         return samples, ids
 
+
 class CUTSTN(tf.keras.Model):
-  def __init__(self, config):
-    super().__init__()
-    self.G = Generator(config)
-    self.D = Discriminator(config)
-    self.F = PatchSampler(config)
-    self.config = config
-  def compile(self,
-              G_optimizer,
-              F_optimizer,
-              D_optimizer):
-      super(CUT, self).compile()
-      self.G_optimizer = G_optimizer
-      self.F_optimizer = F_optimizer
-      self.D_optimizer = D_optimizer
-      self.nce_loss_func = PatchNCELoss(self.tau)
-  
-  @tf.function
-  def train_step(self, inputs):
-    la, xb = inputs
-    
-    with tf.GradientTape(persistent=True) as tape:
-      #synthesize texture
-      z = tf.random.normal(la.shape)
-      xab = self.G([la, z])
-      
-      #discrimination
-      critic_fake = self.D(xab, training=True)
-      critic_real = self.D(xb, training=True)
-      
-      ###compute losses
-      d_loss, g_loss_ = gan_loss(critic_real, critic_fake, self.gan_mode)
-      nce_loss = self.nce_loss_func(la, xab, self.G.E, self.F)
-      g_loss = g_loss_ + self.config['lambda_nce'] * nce_loss
-      
-    G_grads = tape.gradient(g_loss, self.G.trainable_weights)
-    D_grads = tape.gradient(d_loss, self.D.trainable_weights)
-    F_grads = tape.gradient(nce_loss, self.F.trainable_weights)
-    
-    self.G_optimizer.apply_gradients(zip(G_grads, self.G.trainable_weights))
-    self.D_optimizer.apply_gradients(zip(D_grads, self.D.trainable_weights))
-    self.F_optimizer.apply_gradients(zip(F_grads, self.F.trainable_weights))
-      
-    del tape
-    
-    return {'g_loss': g_loss_, 'd_loss':d_loss, 'nce': nce_loss}
-      
-  @tf.function
-  def test_step(self, inputs):
-    la, xb = inputs
-    z = tf.random.normal(la.shape)
-    xab = self.G([la, z]) 
-    nce_loss = self.nce_loss_func(la, xab, self.G.E, self.F)
-    return {'nce': nce_loss}
-  
-  
-  
+    def __init__(self, config):
+        super().__init__()
+        self.G = Generator(config)
+        self.D = Discriminator(config)
+        self.F = PatchSampler(config)
+        self.config = config
+
+    def compile(self,
+                G_optimizer,
+                F_optimizer,
+                D_optimizer):
+        super(CUT, self).compile()
+        self.G_optimizer = G_optimizer
+        self.F_optimizer = F_optimizer
+        self.D_optimizer = D_optimizer
+        self.nce_loss_func = PatchNCELoss(self.tau)
+
+    @tf.function
+    def train_step(self, inputs):
+        la, xb = inputs
+
+        with tf.GradientTape(persistent=True) as tape:
+            # synthesize texture
+            z = tf.random.normal(la.shape)
+            xab = self.G([la, z])
+
+            # discrimination
+            critic_fake = self.D(xab, training=True)
+            critic_real = self.D(xb, training=True)
+
+            ###compute losses
+            d_loss, g_loss_ = gan_loss(critic_real, critic_fake, self.gan_mode)
+            nce_loss = self.nce_loss_func(la, xab, self.G.E, self.F)
+            g_loss = g_loss_ + self.config['lambda_nce'] * nce_loss
+
+        G_grads = tape.gradient(g_loss, self.G.trainable_weights)
+        D_grads = tape.gradient(d_loss, self.D.trainable_weights)
+        F_grads = tape.gradient(nce_loss, self.F.trainable_weights)
+
+        self.G_optimizer.apply_gradients(zip(G_grads, self.G.trainable_weights))
+        self.D_optimizer.apply_gradients(zip(D_grads, self.D.trainable_weights))
+        self.F_optimizer.apply_gradients(zip(F_grads, self.F.trainable_weights))
+
+        del tape
+
+        return {'g_loss': g_loss_, 'd_loss': d_loss, 'nce': nce_loss}
+
+    @tf.function
+    def test_step(self, inputs):
+        la, xb = inputs
+        z = tf.random.normal(la.shape)
+        xab = self.G([la, z])
+        nce_loss = self.nce_loss_func(la, xab, self.G.E, self.F)
+        return {'nce': nce_loss}
+
+
