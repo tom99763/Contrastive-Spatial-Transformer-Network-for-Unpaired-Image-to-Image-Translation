37568bdd820a35a44f094f87638871c486b16a16
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-11 10:06:59 +0800

    Update losses.py

--- models/losses.py
+++ models/losses.py
@@ -31,10 +31,8 @@ class PatchNCELoss:
 
     def __call__(self, source, target, netE, netF, z=None):
         if z is not None:
-            z = z[:, None, None, :]
-            z = tf.repeat(tf.repeat(z, source.shape[1], axis=1), source.shape[2], axis=2)
             feat_source = netE(tf.concat([source, z], axis=-1), training=True)
-            feat_target = netE(tf.concat([target, tf.zeros_like(z)], axis=-1), training=True)
+            feat_target = netE(tf.concat([source, target], axis=-1), training=True)
         else:
             feat_source = netE(source, training=True)
             feat_target = netE(target, training=True)
