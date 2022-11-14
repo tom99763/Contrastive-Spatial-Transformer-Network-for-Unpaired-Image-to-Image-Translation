6b9a941cdad264fed84370c6c31574a37338e3e8
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-14 22:06:21 +0800

    Update and rename visualize.py to visualize_tools.py

--- experiments/visualize.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras import callbacks
-
-def learned_patch_relation(x, E, target_id):
-  pass
-    
-  
-def quantitive_visualize(x, model, num_samples):
-  pass
---
+++ experiments/visualize_tools.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow.keras import callbacks
+import matplotlib.pyplot as plt
+
+def learned_patch_relation(x, E, target_id):
+  feature_maps = E(x)
+  fig, ax = plt.subplots()
+    
+  
+def quantitive_visualize(x, model, opt, params):
+  #y.shape[0]=15
+  y = model.G(x)
+  fig, ax = plt.subplots(ncols = 5, nrows = 6, figsize=(12, 12))
+  
+  k=0
+  for i in range(3):
+    for j in range(5):
+      ax[2*i, j].imshow(x[k] * 0.5 + 0.5)
+      ax[2*i, j].axis('off')
+      ax[2*i+1, j].imshow(y[k] * 0.5 + 0.5)
+      ax[2*i+1, j].axis('off')
+      k+=1
+  plt.savefig(f'{opt.output_dir}/{opt.model}/params.png')
