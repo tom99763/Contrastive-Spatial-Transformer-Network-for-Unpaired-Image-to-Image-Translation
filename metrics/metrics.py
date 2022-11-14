6ac4f75d40df1a2c320f86121e6087ff3da56c19
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-14 18:57:15 +0800

    Update metrics.py

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -2,25 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import callbacks
 from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
 import pandas as pd
-
-def calculate_fid(Eb, Eab):
-    # calculate mean and covariance statistics
-    mu1, sigma1 = Eb.mean(axis=0), np.cov(Eb, rowvar=False)
-    mu2, sigma2 = Eab.mean(axis=0), np.cov(Eab, rowvar=False)
-
-    # calculate sum squared difference between means
-    ssdiff = np.sum((mu1 - mu2) ** 2.0)
-
-    # calculate sqrt of product between cov
-    covmean = sqrtm(sigma1.dot(sigma2))
-
-    # check and correct imaginary numbers from sqrt
-    if np.iscomplexobj(covmean):
-        covmean = covmean.real
-    # calculate score
-    fid = ssdiff + np.trace(sigma1 + sigma2 - 2.0 * covmean)
-    return fid
-
+from fid import calculate_fid
 
 class MetricsCallbacks(callbacks.Callback):
     def __init__(self, val_data, opt, params):
