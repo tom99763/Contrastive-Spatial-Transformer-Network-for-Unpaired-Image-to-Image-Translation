3264e6782a18426a87af52069a44378797dc2194
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-15 20:06:52 +0800

    Update InfofMatch.py

--- models/InfofMatch.py
+++ models/InfofMatch.py
@@ -5,20 +5,64 @@ from losses import *
 from tensorflow.keras.applications.vgg16 import VGG16
 
 class CoordPredictor(tf.keras.Model):
-  def __init__(self, conifg):
+  def __init__(self, config):
     super().__init__()
+    self.act = config['act']
+    self.use_bias = config['use_bias']
+    self.norm = config['norm']
+    self.num_downsampls = config['num_downsamples']
+    self.num_resblocks = config['num_resblocks']
+    dim = config['base']
+    
+    self.blocks = tf.keras.Sequential([
+        Padding2D(3, pad_type='reflect'),
+        ConvBlock(dim, 7, padding='valid', use_bias=self.use_bias, norm_layer=self.norm, activation=self.act),
+    ])
+    
+    for _ in range(self.num_downsampls):
+      dim = dim  * 2
+      self.blocks.add(ConvBlock(dim, 3, strides=2, padding='same', use_bias=self.use_bias, norm_layer=self.norm, activation=self.act))
+      
+    for _ in range(self.num_resblocks):
+      self.blocks.add(ResBlock(dim, 3, self.use_bias, self.norm))
+      
+    for _ in range(self.num_downsampls):
+      dim  = dim / 2
+      self.blocks.add(ConvTransposeBlock(dim, 3, strides=2, padding='same',
+                                         use_bias=self.use_bias, norm_layer=self.norm, activation=self.act))
+    self.blocks.add(Padding2D(3, pad_type='reflect'))
+    self.blocks.add(ConvBlock(2, 7, padding='valid', activation='tanh'))
+    
+  def call(self, inputs):
+    x = tf.concat(inputs, axis=-1)
+    return self.blocks(x)
+ 
+
+class Encoder(tf.keras.Model):
+  def __init__(self, config):
+    super().__init__()
+    self.nce_layers=config['nce_layers']
+    self.vgg = self.build_vgg()
     
   def call(self, x):
-    return
+    return self.vgg(x)
   
-def Encoder():
-  pass
+  def build_vgg(self):
+    vgg = VGG16(include_top=False)
+    vgg.trainable=False
+    outputs = [vgg.layers[idx].output for idx in self.nce_layers]
+    return tf.keras.Model(inputs=vgg.input, outputs=outputs)
+  
 
 class GridSampler:
   pass
 
+
+
 class PatchSampler:
   pass
+
+
 
 class InfoMatchNet(tf.keras.Model):
   def __init__(self, config):
