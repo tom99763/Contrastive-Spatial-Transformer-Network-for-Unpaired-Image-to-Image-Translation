97c0a22c55f57e6969805c72e606dae70e95dfe3
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-20 19:56:29 +0800

    Update CycleGAN.py

--- models/CycleGAN.py
+++ models/CycleGAN.py
@@ -61,7 +61,40 @@ class CycleGAN(tf.keras.Model):
 
     @tf.function
     def train_step(self, inputs):
-        xa, xb = inputs}
+        xa, xb = inputs
+        
+        with tf.GradientTape(persistent=True) as tape:
+          xaa = self.Ga(xa)
+          xba = self.Ga(xb)
+          xbb = self.Gb(xb)
+          xab = self.Gb(xa)
+          
+          #cyclic
+          xaba = self.Ga(xab)
+          xbab = self.Gb(xba)
+          
+          #discrimination
+          critic_real_a = self.Da(xa)
+          critic_fake_a = self.Da(xba)
+          critic_real_b = self.Db(xb)
+          critic_fake_b = self.Da(xab)
+          
+          critic_real = tf.concat([critic_real_a, critic_real_b], axis=0)
+          critic_fake = tf.concat([critic_fake_a, critic_fake_b], axis=0)
+          
+          ###compute loss
+          d_loss, g_loss = gan_loss(critic_real, critic_fake, self.config['gan_mode'])
+          l_r = l1_loss(xa, xaa) + l1_loss(xb, xbb)
+          l_cycle = l1_loss(xa, xaba) + l1_loss(xb, xbab)
+          l_g = g_loss + l_r + l_cycle
+          l_d = d_loss
+          
+        Ggrads = tape.gradient(l_g, self.Ga.trainable_weights + self.Gb.trainable_weights)
+        Dgrads = tape.gradient(l_d, self.Da.trainable_weights + self.Db.trainable_weights)
+        self.G_optimizer.apply_gradients(zip(Ggrads, self.Ga.trainable_weights + self.Gb.trainable_weights))
+        self.D_optimizer.apply_gradients(zip(Dgrads, self.Da.trainable_weights + self.Db.trainable_weights))
+        return {'l_r':l_r, 'l_cycle':l_cycle, 'g_loss':g_loss, 'd_loss':d_loss}
+          
 
     @tf.function
     def test_step(self, inputs):
