aacb9d0da6fbfb6185fedee2b25d12218c8d8f25
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-23 16:11:44 +0800

    Update modules.py

--- models/modules.py
+++ models/modules.py
@@ -1,9 +1,11 @@
 from tensorflow.keras import layers
 import tensorflow as tf
 
+
 class Padding2D(layers.Layer):
     """ 2D padding layer.
     """
+
     def __init__(self, padding=(1, 1), pad_type='constant', **kwargs):
         assert pad_type in ['constant', 'reflect', 'symmetric']
         super(Padding2D, self).__init__(**kwargs)
@@ -26,13 +28,13 @@ class InstanceNorm(layers.Layer):
         super(InstanceNorm, self).__init__(**kwargs)
         self.epsilon = epsilon
         self.affine = affine
- 
+
     def build(self, input_shape):
         if self.affine:
             self.gamma = self.add_weight(name='gamma',
-                                        shape=(input_shape[-1],),
-                                        initializer=tf.random_normal_initializer(0, 0.02),
-                                        trainable=True)
+                                         shape=(input_shape[-1],),
+                                         initializer=tf.random_normal_initializer(0, 0.02),
+                                         trainable=True)
             self.beta = self.add_weight(name='beta',
                                         shape=(input_shape[-1],),
                                         initializer=tf.zeros_initializer(),
@@ -44,43 +46,44 @@ class InstanceNorm(layers.Layer):
         if self.affine:
             return self.gamma * x + self.beta
         return x
-    
+
+
 class LayerInstanceNorm(layers.Layer):
     def __init__(self, adaptive=False):
         super().__init__()
-        self.in = InstanceNorm(affine=False)
+        self.inst = InstanceNorm(affine=False)
         self.ln = layers.LayerNormalization()
-        
+
         self.rho = tf.Variable(tf.constant(1.0),
-                   constraint= lambda x: tf.clip_by_value(x, 0.0, 1.0))
-        
-        self.adaptive=adaptive
-        
+                               constraint=lambda x: tf.clip_by_value(x, 0.0, 1.0))
+
+        self.adaptive = adaptive
+
     def build(self, shape):
-        dim = shape[0][-1]
         if self.adaptive:
+            dim = shape[0][-1]
             self.gamma = layers.Dense(dim)
             self.beta = layers.Dense(dim)
-        
+
     def call(self, inputs):
         if self.adaptive:
             x, w = inputs
         else:
             x = inputs
-        x_in = self.in(x)
+        x_in = self.inst (x)
         x_ln = self.ln(x)
-        rho = tf.clip_by_value(rho - 0.1, 0.0, 1.0)
+        rho = tf.clip_by_value(self.rho - 0.1, 0.0, 1.0)
         x = rho * x_in + (1 - rho) * x_ln
         if self.adaptive:
             x = self.gamma(w) * x + self.beta(w)
         return x
- 
+
 
 class ConvBlock(layers.Layer):
     def __init__(self,
                  filters,
                  kernel_size,
-                 strides=(1,1),
+                 strides=(1, 1),
                  padding='valid',
                  use_bias=True,
                  norm_layer=None,
@@ -89,11 +92,11 @@ class ConvBlock(layers.Layer):
         super(ConvBlock, self).__init__(**kwargs)
         initializer = tf.random_normal_initializer(0., 0.02)
         self.conv2d = layers.Conv2D(filters,
-                             kernel_size,
-                             strides,
-                             padding,
-                             use_bias=use_bias,
-                             kernel_initializer=initializer)
+                                    kernel_size,
+                                    strides,
+                                    padding,
+                                    use_bias=use_bias,
+                                    kernel_initializer=initializer)
         if activation != 'none':
             self.activation = layers.Activation(activation)
         else:
@@ -108,16 +111,16 @@ class ConvBlock(layers.Layer):
             self.normalization = LayerInstanceNorm(True)
         else:
             self.normalization = tf.identity
-            
-        self.norm_layer=norm_layer
+
+        self.norm_layer = norm_layer
 
     def call(self, inputs, training=None):
         if self.norm_layer == 'adaptive_layer_instance':
             x, w = inputs
         else:
-            x=inputs
-        x = self.conv2d(inputs)
-        
+            x = inputs
+        x = self.conv2d(x)
+
         if self.norm_layer == 'adaptive_layer_instance':
             x = self.normalization([x, w])
         else:
@@ -125,14 +128,15 @@ class ConvBlock(layers.Layer):
         x = self.activation(x)
         return x
 
-    
+
 class ConvTransposeBlock(layers.Layer):
     """ ConvTransposeBlock layer consists of Conv2DTranspose + Normalization + Activation.
     """
+
     def __init__(self,
                  filters,
                  kernel_size,
-                 strides=(1,1),
+                 strides=(1, 1),
                  padding='valid',
                  use_bias=True,
                  norm_layer=None,
@@ -141,11 +145,11 @@ class ConvTransposeBlock(layers.Layer):
         super(ConvTransposeBlock, self).__init__(**kwargs)
         initializer = tf.random_normal_initializer(0., 0.02)
         self.convT2d = layers.Conv2DTranspose(filters,
-                                       kernel_size,
-                                       strides,
-                                       padding,
-                                       use_bias=use_bias,
-                                       kernel_initializer=initializer)
+                                              kernel_size,
+                                              strides,
+                                              padding,
+                                              use_bias=use_bias,
+                                              kernel_initializer=initializer)
         self.activation = layers.Activation(activation)
         if norm_layer == 'batch':
             self.normalization = layers.BatchNormalization()
@@ -188,6 +192,7 @@ class ResBlock(layers.Layer):
                                      norm_layer=norm_layer)
         self.filters = filters
         self.norm_layer = norm_layer
+
     def build(self, shape):
         dim = shape[-1]
         if dim != self.filters:
@@ -203,15 +208,15 @@ class ResBlock(layers.Layer):
         if self.norm_layer == 'adaptive_layer_instance':
             x, w = inputs
             skip = self.skip(x)
-            x = self.reflect_pad1(inputs)
+            x = self.reflect_pad1(x)
             x = self.conv_block1([x, w])
             x = self.reflect_pad2(x)
             x = self.conv_block2([x, w])
         else:
             x = inputs
             skip = self.skip(x)
-            x = self.reflect_pad1(inputs)
+            x = self.reflect_pad1(x)
             x = self.conv_block1(x)
             x = self.reflect_pad2(x)
             x = self.conv_block2(x)
-        return x + self.skip(inputs)
+        return x + skip
