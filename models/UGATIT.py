5bff7e84d209bfb9575535bf191ea36ffbf60da1
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-23 11:15:27 +0800

    Update UGATIT.py

--- models/UGATIT.py
+++ models/UGATIT.py
@@ -41,14 +41,17 @@ class Decoder(tf.keras.Model):
     self.norm = config['norm']
     self.num_downsampls = config['num_downsamples']
     self.num_resblocks = config['num_resblocks']
+    self.num_mlps = config['num_mlps']
     dim = config['max_filters']  
     
     #cam 
     self.gap=layers.GloablAveragePooling2D()
-    self.gmp=layers.GlobalMaxPooling2D()
+    self.gmp=layers.GlobalMaxPool2D()
     self.w = tf.Variable(tf.random.normal([dim, 1]), trainable=True)
     self.fuse = ConvBlock(dim, 1, padding='valid', 
                           use_bias=self.use_bias, activation=self.act)
+    self.wMap = tf.keras.Sequential([layers.Dense(dim, activation = self.act) for _ in range(self.num_mlps-1)])
+    self.wMap.add(layers.Dense(dim))
     
     #upsample
     self.resblocks=[Resblock(dim ,3, self.use_bias, 'adaptive_layer_instance') for _ in range(self.num_resblocks)]
@@ -60,6 +63,9 @@ class Decoder(tf.keras.Model):
                                          use_bias=self.use_bias, norm_layer='layer_instance', activation=self.act))
     self.blocks.add(Padding2D(3, pad_type='reflect'))
     self.blocks.add(ConvBlock(opt.num_channels, 7, padding='valid', activation='tanh'))
+    
+    #wmap
+    self.wMap = tf.keras.Sequential([layers.Dense(config[''])])
   
   def call(self, x):
     w, cam_logits = self.cam(x)
@@ -72,27 +78,28 @@ class Decoder(tf.keras.Model):
     #global average pooling
     cam_gap = self.gap(x)
     cam_gap_logits, cam_gap_weights = self.cMap(cam_gap)
-    x_gap = x * cam_gap_weights
+    x_gap = x * cam_gap_weights #(b, h, w, c)
     
     #global maximam pooling
     cam_gmp = self.mlp(x)
     cam_gmp_logits, cam_gmp_weights = self.cMap(cam_gmp)
-    x_gmp = x * cam_gmp_weights
+    x_gmp = x * cam_gmp_weights #(b, h, w, c)
     
     #fusion
     cam_logits = tf.concat([cam_gap_logits, cam_gmp_logits], axis=-1) #(b, hw, 2)
-    x = tf.concat([x_gap, x_gmp], axis=-1)
-    x = self.fuse(x)
+    x = tf.concat([x_gap, x_gmp], axis=-1) 
+    x = self.fuse(x) #(b, h, w, c)
     
     #wmap
+    x = self.gap(x)
     w = self.wMap(x)
-    return w, cam_logits 
+    return w, cam_logits #(b, d), (b, hw, 2)
   
   def cMap(self, x):
     b, h, w, c = x.shape
     x= layers.Flatten()(x) #(b, hw, c)
     x = x @ self.w #(b, hw, 1)
-    w = tf.gather(tf.transpose(self.w), 0)
+    w = tf.gather(tf.transpose(self.w), 0) #(dim, )
     return x, w
     
 
