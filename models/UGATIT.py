f6a837325bcf027585cd7ba6da3a0f84738a368f
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-23 08:59:33 +0800

    Update UGATIT.py

--- models/UGATIT.py
+++ models/UGATIT.py
@@ -1,1 +1,8 @@
+import sys
 
+sys.path.append('./models')
+from modules import *
+from losses import *
+from discriminators import Discriminator
+import tensorflow as tf
+from tensorflow.keras import layers
