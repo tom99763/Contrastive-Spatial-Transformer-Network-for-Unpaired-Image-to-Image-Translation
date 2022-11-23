124be2449414b9f11ad938faa8aeac35145ad81a
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-23 15:45:11 +0800

    Update UGATIT.py

--- models/UGATIT.py
+++ models/UGATIT.py
@@ -74,13 +74,13 @@ class Decoder(tf.keras.Model):
     #average
     gap = self.gap(x)
     gap_logits = self.gap_fc(gap)
-    gap_weights = self.gap_fc.trainable_weights[0][:, 0] #[dim ,1]
+    gap_weights = self.gap_fc.trainable_weights[0][:, 0] 
     x_gap = x * gap_weights
     
     #maximum
     gmp = self.gap(x)
     gmp_logits = self.gmp_fc(gmp)
-    gmp_weights = self.gmp_fc.trainable_weights[0][:, 0] #[dim ,1]
+    gmp_weights = self.gmp_fc.trainable_weights[0][:, 0]
     x_gmp = x * gmp_weights
     
     #output
@@ -110,6 +110,7 @@ class UGATIT(tf.keras.Model):
     self.Gb = Generator(config, opt)
     self.Da = Discriminator(config)
     self.Db = Discriminator(config)
+    self.config = config
     
     def compile(self,
                 Ga_optimizer,
@@ -128,7 +129,73 @@ class UGATIT(tf.keras.Model):
       xa, xb = inputs
       
       with tf.GradientTape(persistent=True) as tape:
-        pass
+        ###forward
+        #identity
+        xaa, cam_logits_aa = self.Ga(xa)
+        xbb, cam_logits_bb = self.Ga(xb)
+        
+        #translation
+        xab, cam_logits_ab = self.Gb(xa)
+        xba, cam_logits_ba = self.Ga(xb)
+        
+        #cyclic
+        xaba, _ = self.Ga(xab)
+        xbab, _ = self.Gb(xba)
+        
+        #discrimination
+        critic_real_a, critic_real_cam_logits_a = self.Da(xa)
+        critic_real_b, critic_real_cam_logits_b = self.Db(xb)
+        critic_fake_a, critic_fake_cam_logits_a = self.Da(xba)
+        critic_fake_b, critic_fake_cam_logits_b = self.Db(xab)
+        
+        ###compute loss
+        #identity
+        l_ra = l1_loss(xa, xaa)
+        l_rb = l1_loss(xb, xbb)
+        
+        #cyclic
+        l_cycle = l1_loss(xa, xaba) + l1_loss(xb, xbab)
+        
+        #cam generator
+        l_ga_cam = bc(tf.ones_like(cam_logits_ba), cam_logits_ba) +\
+                   bc(tf.zeros_like(cam_logits_aa), cam_logits_aa)
+        
+        l_gb_cam = bc(tf.ones_like(cam_logits_ab), cam_logits_ab) +\
+                   bc(tf.zeros_like(cam_logits_bb), cam_logits_bb)
+        
+        #cam discriminator
+        l_da_cam = bc(tf.ones_like(critic_real_cam_logits_a), critic_real_cam_logits_a) + \
+                   bc(tf.zeros_like(critic_fake_cam_logits_a), critic_fake_cam_logits_a)
+        l_db_cam = bc(tf.ones_like(critic_real_cam_logits_b), critic_real_cam_logits_b) + \
+                   bc(tf.zeros_like(critic_fake_cam_logits_b), critic_fake_cam_logits_b)
+        
+        l_dga_cam = bc(tf.ones_like(critic_fake_cam_logits_a), critic_fake_cam_logits_a)
+        l_dgb_cam = bc(tf.ones_like(critic_fake_cam_logits_b), critic_fake_cam_logits_b)
+        
+        #adversarial loss
+        da_loss, ga_loss = gan_loss(critic_real_a, critic_fake_a, self.config['gan_mode'])
+        db_loss, gb_loss = gan_loss(critic_real_b, critic_fake_b, self.config['gan_mode'])
+        
+        #total loss
+        l_ga = l_ra + l_cycle + l_ga_cam + l_dga_cam + ga_loss
+        l_gb = l_rb + l_cycle + l_gb_cam + l_dgb_cam + gb_loss
+        l_da = l_da_cam + da_loss
+        l_db = l_db_cam + db_loss
+
+      Gagrads = tape.gradient(ga_loss, self.Ga.trainable_weights)
+      Gbgrads = tape.gradient(gb_loss, self.Gb.trainable_weights)
+      Dagrads = tape.gradient(da_loss, self.Da.trainable_weights)
+      Dbgrads = tape.gradient(db_loss, self.Db.trainable_weights)
+
+      self.Ga_optimizer.apply_gradients(zip(Gagrads, self.Ga.trainable_weights))
+      self.Gb_optimizer.apply_gradients(zip(Gbgrads, self.Gb.trainable_weights))
+      self.Da_optimizer.apply_gradients(zip(Dagrads, self.Da.trainable_weights))
+      self.Db_optimizer.apply_gradients(zip(Dbgrads, self.Db.trainable_weights))
+      
+      return {'l_r':0.5 *(l_ra + l_rb), 'l_cycle':l_cycle, 'l_g_cam': 0.5 * (l_ga_cam + l_gb_cam),
+              'l_d_cam' : 0.5 * (l_da_cam + l_db_cam), 'l_dg':0.5 * (l_dga_cam + l_dgb_cam), 
+              'g_loss':0.5 * (l_ga + l_gb), 'd_loss': 0.5 * (l_da + l_db)
+             }
       
       
     def test_step(self, inputs):
