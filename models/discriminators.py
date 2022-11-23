fa2babf59b41eae24cfa644e1758bbc5069e352a
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-11-23 11:47:00 +0800

    Update discriminators.py

--- models/discriminators.py
+++ models/discriminators.py
@@ -1,9 +1,10 @@
 from modules import *
 import tensorflow as tf
+from tensorflow.keras import layers
 
 
 class Discriminator(tf.keras.Model):
-  def __init__(self, config, *kwargs):
+  def __init__(self, config):
     super().__init__()
     disc_type = config['disc_type']
     
@@ -17,7 +18,7 @@ class Discriminator(tf.keras.Model):
       self.disc = Perceptual_Discriminator(config)
       
     elif disc_type == 'cam':
-      self.disc = CAM_Discriminator(config, n_layer)
+      self.disc = CAM_Discriminator(config)
     
     elif disc_type == 'Multi_scale':
       self.disc = None
@@ -54,26 +55,41 @@ class Patch_Discriminator(tf.keras.Model):
   
   
 class CAM_Discriminator(tf.keras.Model):
-  def __init__(self, config, n_layer=5):
+  def __init__(self, config):
     self.act = config['act']
     self.use_bias = config['use_bias']
     self.norm = config['norm']
+    self.num_downsampls = config['num_downsamples']
     self.num_resblocks = config['num_resblocks']
     dim = config['base']
     
+    #discriminator
     self.blocks = tf.keras.Sequential([
       ConvBlock(dim, 4, strides=2, padding='same', use_bias=self.use_bias, norm_layer=self.norm, activation=tf.nn.leaky_relu)
     ])
-    
-    for _ in range(n_layer-3):
+    for _ in range(self.num_downsampls):
       dim = dim * 2
       self.blocks.add(ConvBlock(dim, 4, strides=2, padding='same', use_bias=self.use_bias, norm_layer=self.norm, activation=tf.nn.leaky_relu))
-      
     self.blocks.add(Padding2D(1, pad_type='constant'))
     self.blocks.add(ConvBlock(512, 4, padding='valid', use_bias=self.use_bias, norm_layer=self.norm, activation=tf.nn.leaky_relu))
-    self.blocks.add(Padding2D(1, pad_type='constant'))
-    self.blocks.add(ConvBlock(1, 4, padding='valid'))
+    
+    #class activation map
+    self.gap = layers.GlobalAveragePooling2D()
+    self.gmp = layers.GlobalMaxPool()
+    self.gap_fc = layers.Dense(1, use_bias=False)
+    self.gmp_fc = layers.Dense(1, use_bias=False)
+    self.fuse = ConvBlock(dim, 1, activation = tf.nn.leaky_relu)
+    self.pad = Padding2D(1, pad_type='constant')
+    self.conv = ConvBlock(1, 4, padding='valid')
     
   def call(self, x):
-    return self.blocks(x)
+    x = self.blocks(x)
+    
+    gap = self.gap(x)
+    gap_logits = self.gap_fc(gap)
+    gap_weight = self.gap_fc.trainable_weights[0]
+    
+    
+    
+    
   
